2f50acac6f1cf3b07e582299496f1129f6b348b3
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2024-06-13 09:28:47 -0700

    update

---
+++ Day_18/Q52.ts
@@ -0,0 +1,16 @@
+//  Question 52: Make a mobile device object: Create a simple way to keep track of a mobile device's features. Make sure to include the brand, model and other important features such as how much storage it's got, its screen size or battery lifetime.
+
+
+
+// Make a mobile device object
+let SmartPhone = {
+	Brand: "OPPO",
+	Model: "A12",
+	other_Features: {
+		Battery_Saving: "Long_Lasting",
+		Sim: "Dual_Sim",
+		Storage: "64GB RAM",
+	},
+};
+
+console.log(SmartPhone);
