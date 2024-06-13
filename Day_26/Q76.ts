2586f30b93efd5e3488441f5201fa02beda3ca73
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2024-06-13 09:45:46 -0700

    update

---
+++ Day_26/Q76.ts
@@ -0,0 +1,9 @@
+//  Question 76: Function parameters and return values: Make a function that takes two numbers as parameters, add them to each other, and calculate the result. Please show how to call this function, and record its results.
+
+
+function add(add1 : number , add2 : number):number{
+	return add1 + add2;
+}
+
+let Calculate = add(10, 9);
+console.log("Addition : ", Calculate);
