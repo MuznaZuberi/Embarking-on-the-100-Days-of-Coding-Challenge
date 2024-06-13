c734628c9658e575c486a7956536793d0fc3766b
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2024-06-13 09:48:58 -0700

    update

---
+++ Day_28/Q82.ts
@@ -0,0 +1,8 @@
+//  Question 82: Find the Length of a String: Write a function that takes a string as an argument and returns the number of characters in the string.
+
+function Length_of_str(a : string):number{
+    return a.length;
+}
+
+let String_Length = Length_of_str("TypeScript");
+console.log("Here is a length of string : ", String_Length);
