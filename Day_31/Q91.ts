fb193b7f3580641e277f32a307268e954e0ced58
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2024-06-13 09:53:24 -0700

    update

---
+++ Day_31/Q91.ts
@@ -0,0 +1,5 @@
+//  Question 91: Create an array of your three favorite fruits and add a new fruit to the end of the array.
+
+let fruits: string[] = ["Apple", "Mango", "Grapes" , "Banana"];
+let add_fruit_end_of_the_array = fruits.push("Pomegranate"); // use push method
+console.log(fruits);
