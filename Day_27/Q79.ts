afaf7e85d6c1069dd02c58f88f3c641f9ba2a6a8
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2024-06-13 09:47:33 -0700

    update

---
+++ Day_27/Q79.ts
@@ -0,0 +1,11 @@
+//   Question 79: Creating and Accessing Object Properties: Create an object representing a vehicle with make, model or year attributes. You must show how you can look at the vehicle's model property.
+
+let Car = {
+	Make: "Toyota",
+	Model: "Corolla",
+	Year: 2019,
+};
+
+
+//  access the model property of the car
+console.log(Car.Model);
