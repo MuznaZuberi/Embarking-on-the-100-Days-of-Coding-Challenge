3de954ca4173c3357a9ffd36204297ce17acc993
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2024-06-16 09:07:13 -0700

    update

---
+++ Day_36/Q106.ts
@@ -0,0 +1,11 @@
+//  Using && Operators
+
+let LeapYear: number = 2024;
+
+console.log(LeapYear % 4 === 0 && LeapYear % 100 === 0 && LeapYear % 400 === 0);
+
+
+
+//  Using || Operators
+
+console.log(LeapYear % 4 === 0 || LeapYear % 100 === 0 || LeapYear % 400 === 0);
