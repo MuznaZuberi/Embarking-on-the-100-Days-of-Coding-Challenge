d4ee20a5191299e893b17a0eaaa7a946ad659e77
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2024-07-03 03:55:14 -0700

    update

---
+++ Day_44/Q130.ts
@@ -0,0 +1,4 @@
+export function Student_Bio(Student_name : string , Student_id: number , Student_qualification : string){
+	return Student_name + " " + Student_id + " " + Student_qualification;
+}
+
