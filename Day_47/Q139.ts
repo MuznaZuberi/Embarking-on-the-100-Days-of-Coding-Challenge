4700a9a0155f7900fc92fa78369ee7ccb2115095
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2024-07-03 06:39:11 -0700

    update

---
+++ Day_47/Q139.ts
@@ -0,0 +1,20 @@
+/ Reserved Word : let
+
+let user_name = "Janifer";
+console.log("User_name :" , user_name);
+
+
+
+// Reserve Word : const
+const age = 21;
+console.log("User_age: " ,age);
+
+
+// Reserve Word : if else
+if(user_name === "Janifer"){
+	console.log(user_name);
+}
+
+else{
+	console.log("Invalid user_name!");
+}
