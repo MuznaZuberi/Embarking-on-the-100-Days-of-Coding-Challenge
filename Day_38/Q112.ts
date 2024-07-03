0ac300311a4f11dc6de370a73b22156bccc9c4a8
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2024-07-03 03:40:54 -0700

    update

---
+++ Day_38/Q112.ts
@@ -0,0 +1,9 @@
+
+let Countries_Capital = new Map<string , string>();
+	Countries_Capital.set("Pakistan" , "Islamabad");
+	Countries_Capital.set("United Kingdom" , "London");
+	Countries_Capital.set("U Arab Emirates" , "Abu Dhabi");
+	
+
+
+console.log(Countries_Capital);
