c8b512633a381dc6708dda8fdd41bd14625b6daa
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2024-07-03 15:44:54 +0500

    update

---
+++ Day_40/Q119.ts
@@ -0,0 +1,12 @@
+//  Question 119: Create a while loop that logs "Hello, World!" 5 times.
+
+
+function While_Loop() {
+	let count: number = 1;
+	while(count <=5){
+		console.log("Hello World!");
+		count++;
+	}
+};
+
+While_Loop();
---
+++ Day_40/Q120.ts
@@ -0,0 +1,11 @@
+//  Question 120: Use a for...of loop to iterate through an array of your favorite movies and log each one.
+
+
+function For_of_loop(){
+	let Favrt_Movies: string[] = ["HomeAlone", "BabiesDayOut", "TrainExpress", "Anabella"];
+	for(let movies of Favrt_Movies){
+		console.log(movies);
+	}
+};
+
+For_of_loop();
