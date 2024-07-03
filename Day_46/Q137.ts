9538265f2b52f5995f6a622bfc095077f8f35f84
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2024-07-03 18:37:53 +0500

    update

---
+++ Day_46/Q137.ts
@@ -0,0 +1,17 @@
+//  Question 137: Implement a try-catch block to handle potential errors in a block of code.
+
+try{
+	//let user_name: string = "Zehan";
+	console.log(user_name);
+
+}
+
+catch(error){
+	console.log("Undefine error");
+}
+
+
+
+
+
+// This code tries to execute a block that throws an error, and the catch block handles the error gracefully.
---
+++ Day_46/Q138.ts
@@ -0,0 +1,12 @@
+//  Question 138: Describe how to use breakpoints in browser developer tools to debug JavaScript code.
+
+
+console.log("Hello World!");
+
+// Breakpoints
+console.log(
+	"TypeScript is a strong based programming language. This is a Superset of JavaScript!"
+);
+
+// After Breakpoints
+console.log("I like TypeScript");
